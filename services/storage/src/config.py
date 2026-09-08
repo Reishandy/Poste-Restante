2cf363400495e9b955f847944b327b373d98e6f0
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-09-08 19:29:02 +0800

    feat: implemented blob TTL

--- services/storage/src/config.py
+++ services/storage/src/config.py
@@ -14,6 +14,7 @@ class Config(BaseSettings):
     MONGO_DB_NAME: str = Field("poste_restante_storage_db", description="Database name")
 
     DEV_MODE: bool = Field(False, description="Development mode flag")
+    BLOB_EXPIRY_SECONDS: int = Field(7_776_000, description="Expiry in seconds for blob storage") # Defaults to 90 Days
 
 
 settings = Config()
