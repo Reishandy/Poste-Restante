c5f9b1b21a9514041c8d8b7bbb536c7f6657d452
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-09-10 13:50:39 +0800

    feat: initialized the iOS XCode project

---
+++ apps/ios/PosteRestante/PosteRestante/PosteRestanteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosteRestanteApp.swift
+//  PosteRestante
+//
+//  Created by Muhammad Akbar Reishandy on 10/09/26.
+//
+
+import SwiftUI
+
+@main
+struct PosteRestanteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
