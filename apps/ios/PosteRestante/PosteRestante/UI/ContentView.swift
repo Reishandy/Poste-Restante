5fe6f5e6ac402457eec178b47454efbc44800031
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-09-10 13:51:19 +0800

    feat: initialized the iOS XCode project (#36)

---
+++ apps/ios/PosteRestante/PosteRestante/UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PosteRestante
+//
+//  Created by Muhammad Akbar Reishandy on 10/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Poste Restante")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
